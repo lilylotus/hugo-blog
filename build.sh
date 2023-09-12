6d26230859342a120806690c5a68fa15a19a764b
lilylotus <lily@nihility.cn>
2023-09-13 01:43:04 +0800

    blog build

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+hugo -b 'https://www.nihility.cn/'
--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# -b 'https://www.nihility.cn/'
-nohup hugo server -e production --disableLiveReload  > hugo.log 2>&1 &
+# -b 'https://www.nihility.cn/' --port 443
+nohup hugo server -e production --disableLiveReload > hugo.log 2>&1 &
